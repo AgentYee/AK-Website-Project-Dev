45d636e3ce3519a435aa6c635d1a446196c2662c
BEN-PC\Ben <benji.lam@hotmail.ca>
2017-07-29 08:53:28 -0400

    -Commiting initial project home page. -Readded the .gitignore file. -Bootstrap and JQuery have been imported

---
+++ AK-Website-Project/AK-Website-Project/Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AK_Website_Project.Models.ViewModels
+{
+    public class HomeViewModel
+    {
+
+    }
+}
